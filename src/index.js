1fe1027723812d84d6437a835484a0fc553873b4
DudeBroNYC <46071179+DudeBroNYC@users.noreply.github.com>
2018-12-24 12:04:40 -0500

    add subcomponents

--- src/index.js
+++ src/index.js
@@ -6,7 +6,95 @@ import { makeData, Logo, Tips } from "./Utils";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
+const columns = [
+  {
+    Header: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        accessor: "firstName"
+      },
+      {
+        Header: "Last Name",
+        id: "lastName",
+        accessor: d => d.lastName
+      }
+    ]
+  },
+  {
+    Header: "Info",
+    columns: [
+      {
+        Header: "Wins",
+        accessor: "wins"
+      },
+      {
+        Header: "Losses",
+        accessor: "losses"
+      }
+    ]
+  },
+  {
+    Header: "Last 10 Matches",
+    columns: [
+      {
+        Header: "Trend",
+        accessor: "trend"
+      }
+    ]
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData()
+    };
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={columns}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          SubComponent={row => {
+            return (
+              <div style={{ padding: "20px" }}>
+                <em>Last 10 Matches</em>
+                <br />
+                <br />
+                <ReactTable
+                  data={data}
+                  columns={columns}
+                  defaultPageSize={3}
+                  showPagination={false}
+                  //SubComponent={row => {
+                  //return (
+                  //<div style={{ padding: "20px" }}>
+                  //Another Sub Component!
+                  //</div>
+                  //);
+                  //}}
+                />
+              </div>
+            );
+          }}
+        />
+        <br />
+        <Tips />
+        <Logo />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
+
+/*class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -57,4 +145,4 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));*/
